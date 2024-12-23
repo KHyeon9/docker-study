8f878a74e62a2e84088d2cafc1f19148598e63de
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2024-12-24 00:16:01 +0900

    리버스 프록시를 사용해서 프론트 엔트 주소를 자동화 하기 실습

---
+++ kub-network-finished/frontend/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import './App.css';
+import TaskList from './components/TaskList';
+import NewTask from './components/NewTask';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = useCallback(function () {
+    fetch('/api/tasks', {
+      headers: {
+        'Authorization': 'Bearer abc'
+      }
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonData) {
+        // 응답 데이터가 예상대로 아닌 경우 기본값 설정
+        // 이 부분이 task가 없는 경우 에러 발생이어서 수정
+        if (!jsonData || !jsonData.tasks) {
+          setTasks([]); // 빈 배열로 설정
+        } else {
+          setTasks(jsonData.tasks);
+        }
+      }) .catch(function (error) {
+        console.error('Error fetching tasks:', error);
+        setTasks([]); // 오류 발생 시 기본값 설정
+      });
+  }, []);
+
+  useEffect(
+    function () {
+      fetchTasks();
+    },
+    [fetchTasks]
+  );
+
+  function addTaskHandler(task) {
+    fetch('/api/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer abc',
+      },
+      body: JSON.stringify(task),
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (resData) {
+        console.log(resData);
+      });
+  }
+
+  return (
+    <div className='App'>
+      <section>
+        <NewTask onAddTask={addTaskHandler} />
+      </section>
+      <section>
+        <button onClick={fetchTasks}>Fetch Tasks</button>
+        <TaskList tasks={tasks} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
